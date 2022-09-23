17eaae46a20efc59e6944bb2400f8f45dbb64418
Abdul Aziz <114148116+userisaziz@users.noreply.github.com>
2022-09-24 01:19:12 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Hompage from "./pages/homepage/hompage";
+
+function App() {
+  return (
+    <div>
+      <Hompage />
+    </div>
+  );
+}
+
+export default App;
